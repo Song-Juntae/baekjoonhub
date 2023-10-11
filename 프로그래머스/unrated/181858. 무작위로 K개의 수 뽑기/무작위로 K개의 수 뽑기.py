2ce25fbda14b7c911e838f096abc737ea3f75917
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-10-11 21:42:45 +0900

    [level unrated] Title: 무작위로 K개의 수 뽑기, Time: 2232.90 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -0,0 +1,8 @@
+def solution(arr, k):
+    memo = []
+    for i in arr:
+        if i not in memo:
+            memo.append(i)
+    if len(memo) < k:
+        memo += [-1]*(k-len(memo))
+    return memo[:k]
