9d3ffbbe3ced7ae95ea5cf45200910dfb4f34394
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-10-25 22:28:16 +0900

    [level unrated] Title: 문자열 밀기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+def solution(A, B):
+    if A == B:
+        return 0
+    if len(A) == 1:
+        return -1
+    for i in range(1,len(A)+1):
+        _ = deque(A)
+        _.rotate(i)
+        if _ == deque(B):
+            return i
+    return -1
