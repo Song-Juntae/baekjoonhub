135ddcc3e74890496bf0f333b23d4f898ef29055
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-11-07 22:14:02 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,12 +1,3 @@
 def solution(n):
-    _ = '수박'
-    index = 0
-    answer = ''
-    for i in range(n):
-        if index == 0:
-            answer += _[index]
-            index = 1
-        elif index == 1:
-            answer += _[index]
-            index = 0
-    return answer
+    _ = '수박'*(n//2 + 1)
+    return _[:n]
