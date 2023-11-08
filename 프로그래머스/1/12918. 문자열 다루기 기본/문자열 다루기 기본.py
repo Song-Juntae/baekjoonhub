2ba2098a4ede864794fccdd025321d2465ad9b23
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-11-08 20:15:20 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,8 +1,2 @@
 def solution(s):
-    lens = len(s)
-    if lens == 4 or lens == 6:
-        for i in s:
-            if i.isdigit() == False:
-                return False
-        return True
-    return False
+    return s.isdigit() and len(s) in [4,6]
