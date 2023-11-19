13c0281f3eb8ca1a9c6af34e5ea9d64bdb56c362
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-11-19 23:00:35 +0900

    [level 1] Title: 덧칠하기, Time: 1.16 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,14 +1,8 @@
 def solution(n, m, section):
-    _sum = 0
     cnt = 1
-    for i,j in zip(section[:-1], section[1:]):
-        _ = j - i
-        if _ <= m:
-            _sum += _
-        elif _ > m:
+    sec = section[0] + m - 1
+    for i in section:
+        if i > sec:
             cnt += 1
-            _ = 0
-        if _sum >= m:
-            cnt += 1
-            _sum = 0
+            sec = i + m - 1
     return cnt
